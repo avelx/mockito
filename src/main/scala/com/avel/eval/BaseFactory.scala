e0b9f02b256d06a6d9ad135a360a864d491b9a59
avelreserch <pavel.oliynyk@gmail.com>
2020-04-03 11:46:07 +0100

    Scala: value classes

--- src/main/scala/com/avel/eval/BaseFactory.scala
+++ src/main/scala/com/avel/eval/BaseFactory.scala
@@ -4,4 +4,20 @@ class BaseFactory {
 
   def compute(a: Int, b: Int) : Int = (a + b) / 2
 
+  def recompute(a: Int, b: Int, c: Int => Int) : Int = c(a + b)
+
+}
+
+// define value class
+class Meter(val value: Double) extends AnyVal {
+  def +(m: Meter): Meter = new Meter(value + m.value)
+  def -(m: Meter): Meter =
+    if (value < m.value)
+      throw new Error("Negative result")
+    else
+      new Meter(value - m.value)
+}
+
+object Meter {
+  def apply(value: Double): Meter = new Meter(value)
 }
--- src/test/scala/BaseFactorySpec.scala
+++ src/test/scala/BaseFactorySpec.scala
@@ -1,16 +1,49 @@
-import com.avel.eval.BaseFactory
+import com.avel.eval._
 import org.mockito.MockitoSugar
 import org.scalatest.flatspec.AnyFlatSpec
 
 class BaseFactorySpec extends AnyFlatSpec with MockitoSugar {
 
-  "Mochito" should "call method with exact arguments" in {
-    val (a, b) = (5, 10)
-    val obj = mock[BaseFactory]
-    obj.compute(a, b)
-    verify(obj).compute( a, b )
+  "Mochito" should "verify method with exact value of arguments" in {
+    val mockObj = mock[BaseFactory]
+    mockObj.compute(5, 10)
+    verify(mockObj).compute(5, 10)
   }
 
+  "Mochito" should "stub method calls" in {
+    val mockObj = mock[BaseFactory]
+    when(mockObj.compute(5, 11)) thenReturn 25
+    assert(mockObj.compute(5, 11) === 25)
+  }
 
+  "Mochito" should "verify function matchers" in {
+    val mockObj = mock[BaseFactory]
+    val double = (x: Int) => x * 2
+    mockObj.recompute(5, 4, double)
+    verify(mockObj) recompute(5, 4, double)
+  }
+
+  "Meter addition" should "return correct result" in {
+    val x = new Meter(2.3)
+    val y = new Meter(3.5)
+    val z = x + y
+    assert(z === Meter(5.8))
+  }
+
+  "Meter subtraction" should "return correct result" in {
+    val x = new Meter(7.3)
+    val y = new Meter(3.5)
+    val z = x - y
+    assert(z === Meter(3.8))
+  }
+
+  "Meter subtraction" should "raise exception" in {
+    val x = new Meter(3.3)
+    val y = new Meter(3.5)
+    val thrown = intercept[Error]{
+      x - y
+    }
+    assert(thrown.getMessage === "Negative result")
+  }
 
 }
